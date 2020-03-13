68f1a08272ac55168c45961d7a7abce905dff489
huazhixixi <huazhi.lun@sjtu.edu.cn>
2020-03-13 17:44:10 +0800

    modify __init__

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
 import numpy as np
+from .signal_define import QamSignal
+from .optics import ConstantGainEdfa,Laser
+from .channel import NonlinearFiber
+from .receiver_dsp import cd_compensation
+from .receiver_dsp import matched_filter
+from .receiver_dsp import  CMA
